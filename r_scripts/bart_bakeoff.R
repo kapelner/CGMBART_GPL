dfddc630d58d8adb63605c081742cdb03543da0e
Adam Kapelner <kapelner@gmail.com>
2012-10-11 01:18:02 -0400

    bakeoff to see new features

--- r_scripts/bart_bakeoff.R
+++ r_scripts/bart_bakeoff.R
@@ -18,32 +18,32 @@ if (FALSE){
 PRINT_TREE_ILLUS = FALSE
 JAVA_LOG = FALSE
 
-run_model_N_times = 1
+run_model_N_times = 50
 real_regression_data_sets = c(
-#	"r_boston"
-#	"r_forestfires"
+	"r_boston",
+	"r_forestfires"
 #	"r_concretedata"
 )
 simulated_data_sets = c(
 #	"univariate_linear",
-#	"bivariate_linear",
-#	"friedman",
+	"bivariate_linear",
+	"friedman",
 #	"simple_tree_structure_sigsq_hundredth",
-#	"simple_tree_structure_sigsq_tenth",
+	"simple_tree_structure_sigsq_tenth",
 #	"simple_tree_structure_sigsq_half",
-#	"simple_tree_structure",
+	"simple_tree_structure",
 #	"simple_tree_structure_sigsq_3",
-	"simple_tree_structure_sigsq_5"
-#	"simple_tree_structure_sigsq_10",
-#	"simple_tree_structure_sigsq_30"
+	"simple_tree_structure_sigsq_5",
+	"simple_tree_structure_sigsq_10",
+	"simple_tree_structure_sigsq_30"
 #	"simple_tree_structure_sigsq_100"
 )
 
 #nice to have data around for testing... should be overwritten for custom runs...
 num_trees_of_interest = c(
-	1
-#	5,
-#	10
+	1,
+	5,
+	10
 )
 num_burn_ins_of_interest = c(
 	2000
