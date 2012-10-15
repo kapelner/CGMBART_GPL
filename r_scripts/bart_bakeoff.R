b7c6054fc5655710234bc3b548b2cfb115c5f65f
jbleich89 <jbleich89@gmail.com>
2012-10-15 00:42:02 -0400

    changed logging function in Regression constructor

--- r_scripts/bart_bakeoff.R
+++ r_scripts/bart_bakeoff.R
@@ -18,18 +18,18 @@ if (FALSE){
 PRINT_TREE_ILLUS = FALSE
 JAVA_LOG = FALSE
 
-run_model_N_times = 20
+run_model_N_times = 10
 real_regression_data_sets = c(
 	"r_boston"
 #	"r_forestfires"
 #	"r_concretedata"
 )
 simulated_data_sets = c(
-#	"univariate_linear",
+	"univariate_linear",
 #	"bivariate_linear",
 	"friedman",
 #	"simple_tree_structure_sigsq_hundredth",
-	"simple_tree_structure_sigsq_tenth",
+#	"simple_tree_structure_sigsq_tenth",
 #	"simple_tree_structure_sigsq_half",
 	"simple_tree_structure",
 #	"simple_tree_structure_sigsq_3",
@@ -41,9 +41,8 @@ simulated_data_sets = c(
 
 #nice to have data around for testing... should be overwritten for custom runs...
 num_trees_of_interest = c(
-	1,
-	5,
-	10
+	25,
+	50
 )
 num_burn_ins_of_interest = c(
 	2000
