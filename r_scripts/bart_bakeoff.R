ccfc4e9e8debe55decb124a9c7630a85183564f6
Adam Kapelner <kapelner@gmail.com>
2012-10-15 00:50:17 -0400

    decomped bakeoff params

--- r_scripts/bart_bakeoff.R
+++ r_scripts/bart_bakeoff.R
@@ -15,43 +15,7 @@ if (FALSE){
 	run_bart_bakeoff()
 }
 
-PRINT_TREE_ILLUS = FALSE
-JAVA_LOG = FALSE
-
-run_model_N_times = 10
-real_regression_data_sets = c(
-	"r_boston"
-#	"r_forestfires"
-#	"r_concretedata"
-)
-simulated_data_sets = c(
-	"univariate_linear",
-#	"bivariate_linear",
-	"friedman",
-#	"simple_tree_structure_sigsq_hundredth",
-#	"simple_tree_structure_sigsq_tenth",
-#	"simple_tree_structure_sigsq_half",
-	"simple_tree_structure",
-#	"simple_tree_structure_sigsq_3",
-	"simple_tree_structure_sigsq_5"
-#	"simple_tree_structure_sigsq_10",
-#	"simple_tree_structure_sigsq_30"
-#	"simple_tree_structure_sigsq_100"
-)
-
-#nice to have data around for testing... should be overwritten for custom runs...
-num_trees_of_interest = c(
-	25,
-	50
-)
-num_burn_ins_of_interest = c(
-	2000
-)
-num_iterations_after_burn_ins_of_interest = c(
-	2000
-)
-alphas_of_interest = c(0.95)
-betas_of_interest = c(2)
+source("r_scripts/bart_bakeoff_params.R")
 
 total_num_runs = (length(real_regression_data_sets) + length(simulated_data_sets)) * 
 		length(num_trees_of_interest) * length(num_burn_ins_of_interest) * 
