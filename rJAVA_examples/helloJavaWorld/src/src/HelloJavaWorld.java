618f19700bccdebd10035a2aa14f8979a3322c11
Adam Kapelner <kapelner@gmail.com>
2013-09-22 16:36:23 -0400

    examples of JAVA-R packages

---
+++ rJAVA_examples/helloJavaWorld/src/src/HelloJavaWorld.java
@@ -0,0 +1,11 @@
+public class HelloJavaWorld {
+   
+  public String sayHello() {
+    String result = new String("Hello Java World!");
+    return result;
+  }
+
+  public static void main(String[] args) {
+  }
+
+} 
