fc465cf5a0a22ccdd18bd4ca9f95f59f1f9532c6
Adam Kapelner <kapelner@gmail.com>
2013-10-29 11:59:36 -0400

    debug messages for Zach

--- src/CGM_BART/CGMBART_05_gibbs_base.java
+++ src/CGM_BART/CGMBART_05_gibbs_base.java
@@ -47,7 +47,7 @@ public abstract class CGMBART_05_gibbs_base extends CGMBART_04_init {
 	}
 
 	protected void GibbsSampleDebugMessage(int t) {
-		if (t == 0 && gibbs_sample_num % 100 == 0){//&& gibbs_sample_num % 100 == 0
+//		if (t == 0 && gibbs_sample_num % 100 == 0){//&& gibbs_sample_num % 100 == 0
 			String message = "Sampling M_" + (t + 1) + "/" + num_trees + " iter " + gibbs_sample_num + "/" + num_gibbs_total_iterations;
 			if (num_cores > 1){
 				message += "  thread: " + (threadNum + 1);
@@ -56,7 +56,7 @@ public abstract class CGMBART_05_gibbs_base extends CGMBART_04_init {
 			long max_mem = Runtime.getRuntime().maxMemory();
 			message += "  mem: " + TreeIllustration.one_digit_format.format(mem_used / 1000000.0) + "/" + TreeIllustration.one_digit_format.format(max_mem / 1000000.0) + "MB";
 			System.out.println(message);
-		}
+//		}
 	}
 
 	protected void SampleMusWrapper(int sample_num, int t) {
