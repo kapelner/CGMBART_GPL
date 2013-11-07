ea20320c519962b93c06233829ae880c9ade2fe8
Adam Kapelner <kapelner@gmail.com>
2013-11-06 22:43:24 -0500

    merge

--- examples/automobile.R
+++ examples/automobile.R
@@ -40,8 +40,8 @@ oos_stats
 
 #build another model with alpha beta to show depth of trees
 
-vars_selected = var_selection_by_permute_response_three_methods(bart_machine, bottom_margin = 10, num_permute_samples=10) ##fix dot printing
-names(vars_selected)
+vs = var_selection_by_permute_response_three_methods(bart_machine, bottom_margin = 10, num_permute_samples=10) ##fix dot printing
+names(vs)
 cv_vars = var_selection_by_permute_response_cv(bart_machine, k_folds = 2, num_reps_for_avg = 5, num_permute_samples = 20, num_trees_pred_cv = 50)
 
 rmse_by_num_trees(bart_machine, num_replicates = 20)
@@ -56,13 +56,13 @@ oos_stats
 
 
 check_bart_error_assumptions(bart_machine_cv)
-plot_convergence_diagnostics(bart_machine_cv)
+plot(bart_machine_cv)
 
 
 calc_credible_intervals(bart_machine_cv, new_data = X[100, ], ci_conf = 0.95)
 calc_prediction_intervals(bart_machine_cv, new_data = X[100, ], pi_conf = 0.95)
 
-investigate_var_importance(bart_machine_cv, num_replicates_for_avg = 100)
+investigate_var_importance(bart_machine_cv, num_replicates_for_avg = 20)
 ints = interaction_investigator(bart_machine_cv, num_replicates_for_avg = 200, bottom_margin = 20)
 
 
@@ -137,7 +137,7 @@ bart_machine_cv$training_data_features_with_missing_features
 oos_stats = k_fold_cv(X, y, use_missing_data = TRUE, k_folds = 10)
 oos_stats
 
-investigate_var_importance(bart_machine, num_replicates_for_avg = 25)
+investigate_var_importance(bart_machine_cv, num_replicates_for_avg = 25)
 
 #now let's do some variable selection
 windows()
@@ -151,25 +151,25 @@ vs_cv = var_selection_by_permute_response_cv(bart_machine_cv)
 #build model on just the vars selected by ptwise
 X_dummified = dummify_data(X)
 
-bart_machine_ptwise_vars = build_bart_machine(X_dummified[, vars_selected$important_vars_pointwise_names], y, verbose = T, debug_log = T, use_missing_data = TRUE)
+bart_machine_ptwise_vars = build_bart_machine(X_dummified[, vs$important_vars_pointwise_names], y, verbose = T, debug_log = T, use_missing_data = TRUE)
 bart_machine_ptwise_vars
 bart_machine_ptwise_vars$training_data_features_with_missing_features
 
-oos_stats_red_mod = k_fold_cv(X_dummified[, vars_selected$important_vars_pointwise_names], y, use_missing_data = TRUE, k_folds = 20)
+oos_stats_red_mod = k_fold_cv(X_dummified[, vs$important_vars_pointwise_names], y, use_missing_data = TRUE, k_folds = 20)
 oos_stats_red_mod
 
-bart_machine_simul_max_vars = build_bart_machine(X_dummified[, vars_selected$important_vars_simul_max_names], y, verbose = T, debug_log = T, use_missing_data = TRUE)
+bart_machine_simul_max_vars = build_bart_machine(X_dummified[, vs$important_vars_simul_max_names], y, verbose = T, debug_log = T, use_missing_data = TRUE)
 bart_machine_simul_max_vars
 bart_machine_simul_max_vars$training_data_features_with_missing_features
 
-oos_stats_red_mod = k_fold_cv(X_dummified[, vars_selected$important_vars_simul_max_names], y, use_missing_data = TRUE, k_folds = 20)
+oos_stats_red_mod = k_fold_cv(X_dummified[, vs$important_vars_simul_max_names], y, use_missing_data = TRUE, k_folds = 20)
 oos_stats_red_mod
 
 interactions = interaction_investigator(bart_machine_simul_max_vars)
 
 #conclusion: did better than original model by cutting out crap
 
-mod = lm(as.formula(paste("y ~ (", paste(vars_selected$important_vars_simul_max_names, collapse = "+"), ")^2")), data = X_dummified)
+mod = lm(as.formula(paste("y ~ (", paste(vs$important_vars_simul_max_names, collapse = "+"), ")^2")), data = X_dummified)
 summary(mod)
 
 
@@ -376,7 +376,10 @@ y = Pima.te[, 8]
 set_bart_machine_num_cores(4)
 init_java_for_bart_machine_with_mem_in_mb(2500)
 
+bart_machine = build_bart_machine(X, y)
+y_hat = predict(bart_machine, X)
 
+#k_fold_cv(X, y)
 #doesn't work
 bart_machine_cv = build_bart_machine_cv(X, y)
 bart_machine_cv
@@ -390,10 +393,57 @@ plot_y_vs_yhat(bart_machine_cv, prediction_intervals = TRUE)
 oos_stats = k_fold_cv(X, y, k_folds = 10)
 oos_stats
 
+
+get_var_counts_over_chain(bart_machine_cv)
+
+get_var_props_over_chain(bart_machine_cv)
+
+cov_importance_test(bart_machine_cv, covariates = c("glu"))
+
+check_bart_error_assumptions(bart_machine_cv)
+hist_sigsqs(bart_machine_cv)
+get_sigsqs(bart_machine_cv)
+
+plot_sigsqs_convergence_diagnostics(bart_machine_cv)
+
+investigate_var_importance(bart_machine_cv)
+
+plot_convergence_diagnostics(bart_machine_cv)
+
+interaction_investigator(bart_machine_cv)
+
+pd_plot(bart_machine_cv, j = "glu")
+
+plot_tree_depths(bart_machine_cv)
+get_tree_depths(bart_machine_cv)
+plot_tree_num_nodes(bart_machine_cv)
+plot_mh_acceptance_reject(bart_machine_cv)
+
+rmse_by_num_trees(bart_machine_cv)
+
+bart_predict_for_test_data(bart_machine_cv, X[1 : 50, ], y[1 : 50])
+
+bart_machine_get_posterior(bart_machine_cv, X[1 : 5, ])
+calc_credible_intervals(bart_machine_cv, X[1 : 5, ])
+calc_prediction_intervals(bart_machine_cv, X[1 : 5, ])
+
+vs = var_selection_by_permute_response_three_methods(bart_machine_cv, bottom_margin = 10, num_permute_samples=10) ##fix dot printing
+names(vs)
+cv_vars = var_selection_by_permute_response_cv(bart_machine_cv, k_folds = 2, num_reps_for_avg = 5, num_permute_samples = 20, num_trees_pred_cv = 50)
+
+
+
+
+
+
+
+
+
+
 #build another model with alpha beta to show depth of trees
 
-vars_selected = var_selection_by_permute_response_three_methods(bart_machine, bottom_margin = 10, num_permute_samples=10) ##fix dot printing
-names(vars_selected)
+vs = var_selection_by_permute_response_three_methods(bart_machine, bottom_margin = 10, num_permute_samples=10) ##fix dot printing
+names(vs)
 cv_vars = var_selection_by_permute_response_cv(bart_machine, k_folds = 2, num_reps_for_avg = 5, num_permute_samples = 20, num_trees_pred_cv = 50)
 
 rmse_by_num_trees(bart_machine, num_replicates = 20)
