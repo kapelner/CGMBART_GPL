a2a979daf1c65eda054fe5c48a55a14574440f2d
Adam Kapelner <kapelner@gmail.com>
2013-11-27 22:39:51 -0500

    documented the Classifier.java class

--- src/CGM_BART/Classifier.java
+++ src/CGM_BART/Classifier.java
@@ -1,80 +1,78 @@
 package CGM_BART;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.logging.StreamHandler;
 
-import AlgorithmTesting.DataSetupForCSVFile;
 import CustomLogging.*;
 
 /**
  * The base class for all machine learning / statistical-learning
  * algorithms. Extend this class to add your own implementation.
  * 
- * @author Adam Kapelner
+ * @author Adam Kapelner and Justin Bleich
  */
 public abstract class Classifier {
 
+	/** The way we represent missing values from within our implementation */
 	public static final double MISSING_VALUE = Double.NaN;
+	/**
+	 * Is this value missing?
+	 * 
+	 * @param x	The value we wish to check if it is missing
+	 * @return	True if the value is missing
+	 */
 	public static boolean isMissing(double x){
 		return Double.isNaN(x);
 	}
 	
-	/** the raw training data consisting of xi = [xi1,...,xiM, yi] that will be used to construct the classifier */
+	/** an array of the raw training data by ROW i.e. consisting of xi = [xi1, ..., xiM, yi] */
 	protected transient ArrayList<double[]> X_y;
+	/** an array of the raw training data by COLUMN i.e. consisting of xj = [x1j, ..., xnj] with the last entry being [y1, ..., yn] */ 
 	protected transient ArrayList<double[]> X_y_by_col;
-	/** just the responses */
+	/** the raw responses */
 	protected transient double[] y_orig;
+	/** the responses transformed (only if necessary) */
 	protected transient double[] y_trans;
-//	/** just the design matrix */
-//	protected transient ArrayList<double[]> X;	
 	/** the number of records in the training set */
 	protected int n;
 	/** the number of features / predictors in the training set */
 	protected int p;
 	
-	/** the name of this classifier */
+	/** the name of this classifier (useful for debugging) */
 	protected String unique_name = "unnamed";
 
-
-	private transient double[] in_sample_residuals;
-	
-	protected static PrintWriter output;
-	protected static final String DEBUG_EXT = ".csv";
-	
-	protected static final String CSVFileFromRName = "bart_data.csv";
-	protected static final String CSVFileFromRDirectory = "datasets";		
+	/** the in sample residuals [e1, ..., en] of this classifier after it has been built and evaluated */
+	private transient double[] in_sample_residuals;		
 
 	
-	/** Serializable happy */
+	/** A dummy constructor which keeps <code>Serializable</code> happy */
 	public Classifier(){}
-	
-	
-	public void setDataToDefaultForRPackage(){
-		DataSetupForCSVFile data = new DataSetupForCSVFile(new File(CSVFileFromRDirectory, CSVFileFromRName), true);
-		setData(data.getX_y());
-	}
-	
-	public void addTrainingDataRow(String[] record_string){
+		
+	/**
+	 * Adds an observation / record to the training data array. The
+	 * observation is converted to doubles and the entries that are 
+	 * unrecognized are converted to {@link MISSING_VALUE}'s.
+	 * 
+	 * @param x_i	The observation / record to be added as a String array.
+	 */
+	public void addTrainingDataRow(String[] x_i){
 		//initialize data matrix if it hasn't been initialized already
 		if (X_y == null){
 			X_y = new ArrayList<double[]>();
 		}
 		
 		//now add the new record
-		final double[] record = new double[record_string.length];
-		for (int i = 0; i < record_string.length; i++){
+		final double[] record = new double[x_i.length];
+		for (int i = 0; i < x_i.length; i++){
 			try {
-				record[i] = Double.parseDouble(record_string[i]);
+				record[i] = Double.parseDouble(x_i[i]);
 			}
 			catch (NumberFormatException e){
 				record[i] = MISSING_VALUE;
@@ -84,28 +82,20 @@ public abstract class Classifier {
 		X_y.add(record);		
 	}
 	
+	/**
+	 * This method finalizes the training data after all
+	 * records have been added via the method {@link addTrainingDataRow}.	
+	 */
 	public void finalizeTrainingData(){
 		setData(X_y);
 	}
 	
-	public double[] getResponses(){
-		double[] ys = new double[n];
-		for (int i = 0; i < n; i++){
-			ys[i] = X_y.get(i)[p];
-		}
-		return ys;
-	}	
-
-	
-	/** 
-	 * adds the data to the classifier - 
-	 * data is always a list of int[]'s - call this 
-	 * before calling {@link #Build() Build()} 
+	/**
+	 * This method sets the training data of this machine learning classifier.
+	 * It also populates many essential fields such as {@link n}, {@link p},
+	 * {@link y_orig}, {@link X_y} and {@link X_y_by_col}.
 	 * 
-	 * WARNING: It is up to the user to supply raw data with
-	 * at least one record, and each record to be in the form
-	 * x_i1, x_i2, ..., x_ip, yi
-	 * 
+	 * @param X_y	The list of double vectors to be set as the training data.
 	 */
 	public void setData(ArrayList<double[]> X_y){
 		n = X_y.size();
@@ -121,6 +111,13 @@ public abstract class Classifier {
 		this.X_y_by_col = getDataMatrixByCol(X_y);
 	}
 	
+	/**
+	 * Given a training data set indexed by row, this produces a training
+	 * data set indexed by column
+	 * 
+	 * @param X_y	The training data set indexed by row
+	 * @return		The training data set indexed by column
+	 */
 	private ArrayList<double[]> getDataMatrixByCol(ArrayList<double[]> X_y) {
 		 ArrayList<double[]> X_y_by_col = new ArrayList<double[]>(n);
 		 for (int j = 0; j < p; j++){
@@ -133,6 +130,13 @@ public abstract class Classifier {
 		 return X_y_by_col;
 	 }
 
+	/** 
+	 * This function tacks on the original index of each observation to a training data set
+	 * tacked on at the end of the observations' vectors.
+	 * 
+	 * @param X_y_old	The original training data set
+	 * @return			The training data set with indices tacked on.
+	 */
 	private ArrayList<double[]> addIndicesToDataMatrix(ArrayList<double[]> X_y_old) {
 		ArrayList<double[]> X_y_new = new ArrayList<double[]>(n);
 		for (int i = 0; i < n; i++){
@@ -147,7 +151,12 @@ public abstract class Classifier {
 		return X_y_new;
 	}
 
-
+	/**
+	 * This provides a vector of responses from the training data set
+	 * 
+	 * @param X_y	The training data set
+	 * @return		The vector of responses
+	 */
 	private double[] extractResponseFromRawData(ArrayList<double[]> X_y) {
 		double[] y = new double[X_y.size()];
 		for (int i = 0; i < X_y.size(); i++){
@@ -157,45 +166,12 @@ public abstract class Classifier {
 		return y;
 	}
 	
-	public static ArrayList<double[]> clone_data_matrix_with_new_y_optional(List<double[]> X_y, double[] y_new){
-		if (X_y == null){
-			return null;
-		}
-		ArrayList<double[]> X_y_new = new ArrayList<double[]>(X_y.size());
-		for (int i = 0; i < X_y.size(); i++){
-			double[] original_record = X_y.get(i);
-			int num_cols = original_record.length;
-			double[] new_record = new double[num_cols];
-			for (int j = 0; j < num_cols; j++){
-				
-				if (j == num_cols - 2 && y_new != null){
-//					System.out.println("clone_data_matrix_with_new_y_optional y_new");
-					new_record[j] = y_new[i];
-				}
-				else {
-					new_record[j] = original_record[j];
-				}
-			}
-			X_y_new.add(new_record);
-
-//			System.out.println("original_record: " + IOTools.StringJoin(original_record, ","));
-//			System.out.println("new_record: " + IOTools.StringJoin(new_record, ","));
-		}
-		return X_y_new; 
-	}	
-	
-	public ArrayList<double[]> getData() {
-		return X_y;
-	}	
-	
-	/** build the machine learning classifier, you must {@link #setData(ArrayList) set the data} first */
+	/** build the machine learning classifier (implemented by a daughter class), you must {@link #setData(ArrayList) set the data} first */
 	public abstract void Build();
 	
-	public static void fixRandSeed(){
-		StatToolbox.setSeed(1984);
-	}
-	
 	/**
+	 * Useful for debugging only. Undocumented.
+	 * 
 	 * @see https://blogs.oracle.com/nickstephen/entry/java_redirecting_system_out_and
 	 */
 	public void suppressOrWriteToDebugLog(){
@@ -225,38 +201,61 @@ public abstract class Classifier {
         System.setErr(new PrintStream(los, true)); 		
 	}
 	
-	/** deletes all data that's unneeded to save memory */
+	/** deletes all data that's unneeded at this point in runtime in order to save memory */
 	protected abstract void FlushData();
 	
-	/** After the classifier has been built, new records can be evaluated */
+	/**
+	 * After the classifier has been built, new records can be evaluated / predicted
+	 * (implemented by a daughter class)
+	 * 
+	 * @param record		The observation to be evaluated / predicted
+	 * @param num_cores		The number of processor cores to be used during the evaluation / prediction
+	 * @return				The prediction
+	 */
 	public abstract double Evaluate(double[] record, int num_cores);
 	
+	/**
+	 * A wrapper for {@link Evaluate(double[], int)} where one processor core is used
+	 * 
+	 * @param record		The observation to be evaluated / predicted
+	 * @return				The prediction
+	 */
 	public double Evaluate(double[] record){
 		return Evaluate(record, 1);
 	}
 	
 	/**
-	 * Given a data record, return the Y value - take the last index
+	 * Given a data record, return the Y (response) value i.e. take the last index
 	 * 
 	 * @param record		the data record
-	 * @return				its y value (class)
+	 * @return				the data record's response value (or class)
 	 */
 	public double getResponseFromRecord(double[] record){
 		return record[p];
 	}
 
-	/** Stop the classifier in its building phase */
+	/** Stop the classifier during its building phase */
 	public abstract void StopBuilding();
 
+	/**
+	 * How many features are in the training data set?
+	 * 
+	 * @return	The number of features in the training data set
+	 */
 	public int getP() {
 		return p;
 	}
 	
+	/**
+	 * How many observations are in the training data set?
+	 * 
+	 * @return	The number of observations in the training data set
+	 */
 	public int getN() {
 		return n;
 	}	
 	
-	//useful for debugging
+	/** Useful for debugging. Undocumented */
 	public void dumpDataToFile(String optional_title){
 		PrintWriter out=null;
 		try {
@@ -282,14 +281,14 @@ public abstract class Classifier {
 		out.close();		
 	}
 	
+	/** a variable that represents the different error calculation types */
 	public static enum ErrorTypes {L1, L2, MISCLASSIFICATION};
+	
 	/**
 	 * Calculates the in-sample error using the specified loss function
-	 * @param num_cores_evaluate 
-	 * @param type_of_error_rate  the loss function 
-	 * @return the error rate
-	 */	
-	
+	 * 
+	 * @param num_cores_evaluate 	The number of processor cores to use
+	 */		
 	private void calculateInSampleResiduals(int num_cores_evaluate){
 		long t0 = System.currentTimeMillis();
 		System.out.print("calculating in-sample residuals...");
@@ -305,6 +304,13 @@ public abstract class Classifier {
 		System.out.print("done in " + ((t1 - t0) / 1000.0) + " sec \n");
 	}
 	
+	/**
+	 * Calculates the in-sample error based on a specified error metric
+	 * 
+	 * @param type_of_error_rate	The error metric to use to compute loss
+	 * @param num_cores_evaluate	The number of processor cores to use
+	 * @return						The in-sample loss as a sum total across all training observations
+	 */
 	public double calculateInSampleLoss(ErrorTypes type_of_error_rate, int num_cores_evaluate){	
 		if (in_sample_residuals == null){
 			calculateInSampleResiduals(num_cores_evaluate);
@@ -330,6 +336,10 @@ public abstract class Classifier {
 		return loss;
 	}
 	
+	/**
+	 * Transforms the response variable (implemented by a daughter class).
+	 * The default here is to just save the original response.
+	 */
 	protected void transformResponseVariable() {
 		y_trans = new double[y_orig.length];
 		//default is to do nothing... ie just copy the y's into y_trans's
@@ -337,18 +347,16 @@ public abstract class Classifier {
 			y_trans[i] = y_orig[i];
 		}		
 	}	
-	
-	protected double un_transform_y(double y_i) {
-		//default:
-		return y_i;
-	}
 
-	public double calculateMisclassificationRate(int num_cores_evaluate){
-		return calculateInSampleLoss(ErrorTypes.MISCLASSIFICATION, num_cores_evaluate) / (double) n * 100;
-	}
-	
-	public double[] getYTrans() {
-		return y_trans;
+	/**
+	 * Untransforms a response value (implemented by a daughter class).
+	 * The default here is to just return the original response.
+	 * 
+	 * @param y_i	The value to untransform
+	 * @return		The untransformed value
+	 */
+	protected double un_transform_y(double y_i) {
+		return y_i;
 	}		
 	
 	public Classifier clone(){
@@ -357,15 +365,5 @@ public abstract class Classifier {
 	
 	public void setUniqueName(String unique_name) {
 		this.unique_name = unique_name;
-	}
-	
-	public void writeStdOutToLogFile(){
-		try {
-			Logger.getLogger("").addHandler(new StreamHandler()); //turn off std out
-			suppressOrWriteToDebugLog();
-		}
-		catch (Error e){
-			System.out.println("Logger and or suppressOrWriteToDebugLog FAILING\n");
-		}		
 	}
 }
